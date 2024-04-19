ea86d71044ac2e2a5a8b221d6cae3b0ad41cc54a
Алексей <zaykuh@gmail.com>
2024-04-19 16:42:37 +0500

    page: add page home

---
+++ src/pages/Home/compose/Home.tsx
@@ -0,0 +1,17 @@
+
+import { Slider } from "@/features/slider";
+import forMen from "@/images/forMen.jpg";
+import forWomen from "@/images/forWomen.jpg";
+import { Link } from "react-router-dom";
+
+export function Home(){
+    return <div className="main">
+        <div className="home-imgs">
+            <Link className="home-imgs_link" to={'men/'}><img className="home-imgs_item" src={forMen} alt="forMen"/><span className="home-imgs_text">for him</span></Link>
+            <Link className="home-imgs_link" to={'women/'}><img className="home-imgs_item" src={forWomen} alt="forWomen" /><span className="home-imgs_text">for her</span></Link>
+        </div>
+        <div className="home-slider">
+            <Slider/>
+        </div>
+    </div>
+}
