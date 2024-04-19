9d9fb9247f17b3b843b8f1e51cf4963e357396ec
Алексей <zaykuh@gmail.com>
2024-04-19 16:44:40 +0500

    widgets: add sidebar

--- src/widgets/sidebar/compose/Sidebar.tsx
+++ src/widgets/sidebar/compose/Sidebar.tsx
@@ -1,12 +1,36 @@
-import { TabList } from "../../../features/tabList/compose/tabList";
-import {ReactComponent as CloseImg} from "../../../images/icon-close.svg"
+import { MouseEvent, useEffect, useRef } from "react";
+import { TabList } from "@/features/index";
+import {ReactComponent as CloseImg} from "@/images/icon-close.svg"
+import { toggleSidebar } from "../model/sidebar-slice";
+import { useDispatch } from "react-redux";
 
-interface ISideabar{
-    handleClick: () => void
-}
+export function Sidebar(){
 
-export function Sidebar({handleClick}: ISideabar){
-    return <div className="sidebar">
+    const dispatch = useDispatch()
+
+    const sidebarRef = useRef<HTMLDivElement>(null)
+
+    function handleClick(){
+        
+        if (window.innerWidth <= 1024){
+            dispatch(toggleSidebar(null))
+        }
+    }
+
+    function handleClickSb(event: MouseEvent<HTMLDivElement> ){
+        if (sidebarRef.current === event.target){
+            document.documentElement.style.overflow = '';
+            dispatch(toggleSidebar(null))
+        }
+    }
+
+    useEffect(()=>{
+        if (sidebarRef.current){
+            sidebarRef.current.focus()
+        }
+    }, [])
+
+    return <div ref={sidebarRef} onClick={handleClickSb} className="sidebar">
         <div className="sidebar-content">
             <div onClick={handleClick}><CloseImg/></div>
             <TabList handleClick={handleClick} className="sidebar"/>
---
+++ src/widgets/sidebar/model/sidebar-slice.tsx
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+export const sidebarSlice = createSlice({
+    name: "@sidebar",
+    initialState: false,
+    reducers: {
+        toggleSidebar: (state, _) =>{
+            return !state
+        }
+    }
+})
+
+export const { toggleSidebar } = sidebarSlice.actions
