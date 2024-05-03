a0cce4b03286c6aa0e4a9bb701c5456e019236ae
Алексей <zaykuh@gmail.com>
2024-05-03 13:59:14 +0500

    widjet HeaderMenu: add logic clearFilters

--- src/widgets/header/ui/HeaderMenu.tsx
+++ src/widgets/header/ui/HeaderMenu.tsx
@@ -4,6 +4,7 @@ import { ReactComponent as MenuImg } from "@/images/icon-menu.svg";
 import { ReactComponent as LogoImg } from "@/images/logo.svg";
 import { toggleSidebar } from "../../sidebar/model/sidebar-slice";
 import { Link } from "react-router-dom";
+import { clearFilters } from "@/features/filterCards/model/filter-cards-slice";
 
 export function HeaderMenu(){
 
@@ -12,10 +13,11 @@ export function HeaderMenu(){
     function handleClick(){
         document.documentElement.style.overflow = 'hidden';
         dispatch(toggleSidebar(null))
+        dispatch(clearFilters(null))
     }
 
     return <div className='header-box header-tablist'>
-        <div onClick={handleClick} className="header-menu"><MenuImg/></div>
+        <div onClick={handleClick} onTouchStart={handleClick} className="header-menu"><MenuImg/></div>
         <Link to={"/sneakers-shop"}><div className="header-logo"><LogoImg/></div></Link>
         <TabList/>
     </div>
