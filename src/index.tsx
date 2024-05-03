357c71848c01aa7a2a2b27841d1a51747d46b96d
Алексей <zaykuh@gmail.com>
2024-05-03 13:12:30 +0500

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,49 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./scss/index.scss"
-import { App } from './app';
-import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { Collections, About, Men, Women, Contact } from './pages';
+
+import { RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import { Home } from './pages/Home';
-import { ErrorPage } from './pages';
-
-const router = createBrowserRouter([
-  {
-    path: "sneakers-shop/",
-    element: <App><Outlet/></App>,
-    errorElement: <App><ErrorPage/></App>,
-    children: [
-      
-      {
-        path:  "",
-        element: <Home />,
-      },
-      {
-        path:  "collections/",
-        element: <Collections />,
-      },
-      {
-        path:  "about/",
-        element: <About />,
-      },
-      { 
-        path:  "men/",
-        element: <Men/>,
-      },
-      { 
-        path:  "women/",
-        element: <Women />,
-      },
-      { 
-        path:  "contact/",
-        element: <Contact/>,
-      },
-    ],
-  },
-]);
-
+import { router } from './routes/router';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
