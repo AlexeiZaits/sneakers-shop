7d91cd9e88b93b96ba4daa645b270f9c26502bec
Алексей <zaykuh@gmail.com>
2024-05-03 13:13:11 +0500

    reducer: add cardList, collectionItem, calcAmount, cartList, filterCardsList

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit"
 import { basketSlice } from "./widgets/basket/model/basket-slice"
 import { sidebarSlice } from "./widgets/sidebar/model/sidebar-slice"
+import { CardListSlice } from "./features/cardList/model/card-list-slice"
+import { collectionItem } from "./pages/CollectionItem/model/collection-item-slice"
+import { calcAmountSlice } from "./features/addToCart/model/calc-amount-lsice"
+import { cartListSlice } from "./features/cartList/model/cart-list-slice"
+import { filterCardsSlice } from "./features/filterCards/model/filter-cards-slice"
+
 
 
 
@@ -8,6 +14,11 @@ export const store = configureStore({
     reducer: {
         basket: basketSlice.reducer,
         sidebar: sidebarSlice.reducer,
+        cardList: CardListSlice.reducer,
+        collectionItem: collectionItem.reducer,
+        calcAmount: calcAmountSlice.reducer,
+        cartList: cartListSlice.reducer,
+        filterCardsList: filterCardsSlice.reducer,
         
     },
     devTools: true,
