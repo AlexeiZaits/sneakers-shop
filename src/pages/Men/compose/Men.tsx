0c141f7581cf712ca42546bc28a5768b57cd628c
Алексей <zaykuh@gmail.com>
2024-05-03 13:38:01 +0500

    page Men: add Links, FilterCards, SortCard, CardList

--- src/pages/Men/compose/Men.tsx
+++ src/pages/Men/compose/Men.tsx
@@ -1,7 +1,17 @@
+import { CardList, FilterCards, SortCard } from "@/features/index"
+import { Links } from "@/shared/index"
+import { useLocation } from "react-router-dom"
 
 
 export function Men(){
+    const location = useLocation()
+    
     return <div className="main">
-        Men
+        <Links pathname={location.pathname}/>
+        <div className="main-container">
+            <FilterCards/>
+            <SortCard/>
+        </div>
+        <CardList filter="man"/>
     </div>
 }
