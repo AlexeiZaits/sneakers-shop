0f22e83c087c63b3ea8d4ad215ed0c651d05a81b
Алексей <zaykuh@gmail.com>
2024-05-03 13:38:34 +0500

    page Women: add Links, FilterCards, SortCard

--- src/pages/Women/compose/Women.tsx
+++ src/pages/Women/compose/Women.tsx
@@ -1,7 +1,17 @@
-
+import { CardList, FilterCards, SortCard } from "@/features/index"
+import { Links } from "@/shared/index"
+import { useLocation } from "react-router-dom"
 
 export function Women(){
+    const location = useLocation()
+    
     return <div className="main">
-        Women
+        <Links pathname={location.pathname}/>
+        <div className="main-container">
+            <FilterCards/>
+            <SortCard/>
+        </div>
+        <CardList filter="woman"/>
     </div>
 }
+    
