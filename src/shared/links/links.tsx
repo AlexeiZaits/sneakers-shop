da1228473aff37c83a4625d47ffe33e5b74f0976
Алексей <zaykuh@gmail.com>
2024-05-05 02:02:06 +0500

    shared(links): refactor

--- src/shared/links/links.tsx
+++ src/shared/links/links.tsx
@@ -13,14 +13,16 @@ export function Links({pathname, name}:ILinks){
     return <div className="links">
         <Link className="links-active" to={`/${links[0]}`}>Home </Link>
         {links.map((item, index)=> {
-            if (item === "" || index === 0){
-                return ""
+            if (index === 0){
+                return null
             }
             if (index === links.length-1){
                 if(name) return <span key={name} className="links-unactive"> {name}</span>
                 return <span key={item[0]}> {item[0].toUpperCase()}{item.slice(1,)}</span>
             }
-            return <Link key={`/${links[0]}/${item}`} className="links-active" to={`/${links[0]}/${item}`}> {item[0].toUpperCase()}{item.slice(1,)} </Link>
+            return <Link key={`/${links[0]}/${item}`} className="links-active" to={`/${links[0]}/${item}`}>
+                {item[0].toUpperCase()}{item.slice(1,)}
+            </Link>
         })}
     </div>
 }
