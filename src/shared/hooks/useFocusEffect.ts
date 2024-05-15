bb94199005bff9227e509da57acf54d43385506d
Алексей <zaykuh@gmail.com>
2024-05-15 14:55:04 +0500

    shared: delete

--- src/shared/hooks/useFocusEffect.ts
+++
@@ -1,7 +0,0 @@
-import { useEffect } from "react"
-
-export function useFocusEffect(ref: React.RefObject<HTMLDivElement>){
-    useEffect(()=> {
-        ref.current?.focus()
-    }, [ref])
-}
--- src/shared/lib/handleBlur.ts
+++
@@ -1,5 +0,0 @@
-import { Dispatch } from 'redux';
- 
-export function handleBlur(state: boolean, toggleState: (payload: any) => any, dispatch: Dispatch<any>) {
-    dispatch(toggleState(false));
-}
